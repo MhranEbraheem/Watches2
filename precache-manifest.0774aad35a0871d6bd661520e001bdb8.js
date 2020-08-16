610b9395ec8eb9c3c8c8c0925f3bcb30e905e39e
MhranEbraheem <61749759+MhranEbraheem@users.noreply.github.com>
2020-08-16 22:20:58 +0300

    Updates

---
+++ precache-manifest.0774aad35a0871d6bd661520e001bdb8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0ea969ddd30f4283145b5be3c4a01b",
+    "url": "/Watches2/index.html"
+  },
+  {
+    "revision": "f9d2e532633821dfdebd",
+    "url": "/Watches2/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "e04f22428064784f9a11",
+    "url": "/Watches2/static/css/main.cf1f245d.chunk.css"
+  },
+  {
+    "revision": "f9d2e532633821dfdebd",
+    "url": "/Watches2/static/js/2.00a8097a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Watches2/static/js/2.00a8097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04f22428064784f9a11",
+    "url": "/Watches2/static/js/main.216462f0.chunk.js"
+  },
+  {
+    "revision": "694350cafb4ee4be585a",
+    "url": "/Watches2/static/js/runtime-main.d253148e.js"
+  },
+  {
+    "revision": "e9763158677688d697af2bc69db4645a",
+    "url": "/Watches2/static/media/armani.e9763158.jpg"
+  },
+  {
+    "revision": "a434259651465ad0d281bd72214ac826",
+    "url": "/Watches2/static/media/boss.a4342596.jpg"
+  },
+  {
+    "revision": "f95cf741814d365ff05bd8aa5d4d3194",
+    "url": "/Watches2/static/media/rado.f95cf741.jpg"
+  },
+  {
+    "revision": "36b78ffa9f82cd017eb4153aeff1f9d9",
+    "url": "/Watches2/static/media/rolex.36b78ffa.jpg"
+  }
+]);
